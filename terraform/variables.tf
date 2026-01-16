a80e60698e18f5e050c9348e7bd79903c0d2c58f
Tony Angerilli <tony@angerilli.ca>
2026-01-16 15:24:02 -0800

    use us-west-2 for deployments

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   type        = string
   description = "AWS region to deploy into."
-  default     = "us-east-1"
+  default     = "us-west-2"
 }
 
 variable "dynamodb_table_name" {
